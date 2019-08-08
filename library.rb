7eae140217bb5d2036e6c5067a00deadff071cf8
Hayden Westfield-Bell <wickertongue@Users-MacBook-Pro.local>
2019-08-08 22:48:37 +0100

    added loan book method which removes book from inventory and into on loan

--- library.rb
+++ library.rb
@@ -5,6 +5,7 @@ class Library
   def initialize(name)
     @name = name
     @inventory = []
+    @books_on_loan = []
   end
 
   def add_book_to_inventory(book)
@@ -14,5 +15,15 @@ class Library
   def count_inventory
     @inventory.count
   end
+
+  def loan_book_to_customer(customer, book)
+    @inventory.delete(book)
+    @books_on_loan.push(book)
+  end
+
+  def count_books_on_loan
+    @books_on_loan.count
+  end
+
 
 end
--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -9,18 +9,29 @@ class LibraryTest < MiniTest::Test
 
   def setup
     @library1 = Library.new("East End Library")
+    @customer1 = Customer.new("Nathan Hawthorne")
     @book1 = Book.new("Pale Fire", "Vladamir Nabokov")
     @book2 = Book.new("Crash", "J G Ballard")
   end
 
-  def test_get_library_name
-    assert_equal("East End Library", @library1.name)
-  end
+  # def test_get_library_name
+  #   assert_equal("East End Library", @library1.name)
+  # end
+  #
+  # def test_add_book_to_inventory__1_book
+  #   @library1.add_book_to_inventory(@book1)
+  #   assert_equal(1, @library1.count_inventory)
+  # end
+  #
+  # def test_add_book_to_inventory__2_books
+  #   @library1.add_book_to_inventory(@book1)
+  #   @library1.add_book_to_inventory(@book2)
+  #   assert_equal(2, @library1.count_inventory)
+  # end
 
-  def test_add_book_to_inventory
-    @library1.add_book_to_inventory(@book1)
-    @library1.add_book_to_inventory(@book2)
-    assert_equal(2, @library1.count_inventory)
+  def test_books_on_loan__1_book
+    @library1.loan_book_to_customer(@customer1, @book1)
+    assert_equal(1, @library1.count_books_on_loan)
   end
 
 end
