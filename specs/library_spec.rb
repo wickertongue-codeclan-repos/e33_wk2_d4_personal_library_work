7bf380cc1035634a678e8134d94aa595bb8f3bba
Hayden Westfield-Bell <wickertongue@Users-MacBook-Pro.local>
2019-08-09 09:28:26 +0100

    added new tests for library.spec

--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -14,20 +14,20 @@ class LibraryTest < MiniTest::Test
     @book2 = Book.new("Crash", "J G Ballard")
   end
 
-  # def test_get_library_name
-  #   assert_equal("East End Library", @library1.name)
-  # end
-  #
-  # def test_add_book_to_inventory__1_book
-  #   @library1.add_book_to_inventory(@book1)
-  #   assert_equal(1, @library1.count_inventory)
-  # end
-  #
-  # def test_add_book_to_inventory__2_books
-  #   @library1.add_book_to_inventory(@book1)
-  #   @library1.add_book_to_inventory(@book2)
-  #   assert_equal(2, @library1.count_inventory)
-  # end
+  def test_get_library_name
+    assert_equal("East End Library", @library1.name)
+  end
+
+  def test_add_book_to_inventory__1_book
+    @library1.add_book_to_inventory(@book1)
+    assert_equal(1, @library1.count_inventory)
+  end
+
+  def test_add_book_to_inventory__2_books
+    @library1.add_book_to_inventory(@book1)
+    @library1.add_book_to_inventory(@book2)
+    assert_equal(2, @library1.count_inventory)
+  end
 
   def test_books_on_loan__1_book
     @library1.loan_book_to_customer(@customer1, @book1)
